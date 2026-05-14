b95b76aaed17bab322d6b7d8564165ac810e63f0
Hemanth Rachapalli <rachapalli.hemanth5544@gmail.com>
2026-05-14 10:27:11 +0530

    db: added auto migrate

--- apps/api/internal/platform/postgres/db.go
+++ apps/api/internal/platform/postgres/db.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log/slog"
 
+	"github.com/wikiora/wikiora/apps/api/internal/domain"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
@@ -20,16 +21,16 @@ func Connect(databaseURL string, appEnv string, log *slog.Logger) (*gorm.DB, err
 		return nil, fmt.Errorf("open database: %w", err)
 	}
 
-	// if err := db.AutoMigrate(
-	// 	&domain.User{},
-	// 	&domain.Workspace{},
-	// 	&domain.WorkspaceMember{},
-	// 	&domain.WorkspaceInvite{},
-	// 	&domain.Query{},
-	// 	&domain.QueryReply{},
-	// ); err != nil {
-	// 	return nil, fmt.Errorf("auto migrate: %w", err)
-	// }
+	if err := db.AutoMigrate(
+		&domain.User{},
+		&domain.Workspace{},
+		&domain.WorkspaceMember{},
+		&domain.WorkspaceInvite{},
+		&domain.Query{},
+		&domain.QueryReply{},
+	); err != nil {
+		return nil, fmt.Errorf("auto migrate: %w", err)
+	}
 
 	log.Info("database connected")
 	return db, nil
